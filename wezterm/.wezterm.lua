4bb6153c5133b87995ea7cf720835b9fd7d06efe
rjleyva <rleyva@tutanota.com>
2025-05-20 17:15:49 -0400

    refactor(wezterm): convert wezterm config to opts table format

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,23 +1,21 @@
 local wezterm = require("wezterm")
 
-local config = wezterm.config_builder()
+return {
+	window_decorations = "RESIZE",
+	enable_tab_bar = false,
+	window_background_opacity = 0.75,
+	macos_window_background_blur = 10,
 
-config.window_decorations = "RESIZE"
-config.enable_tab_bar = false
-config.window_background_opacity = 0.75
-config.macos_window_background_blur = 10
+	color_scheme = "tokyonight_night",
+	max_fps = 120,
 
-config.color_scheme = "tokyonight_night"
-config.max_fps = 120
+	font = wezterm.font("PlemolJP Console NF"),
+	font_size = 18,
 
-config.font = wezterm.font("PlemolJP Console NF")
-config.font_size = 18
-
-config.font_rules = {
-	{
-		italic = true,
-		font = wezterm.font("PlemolJP Console NF", { italic = true }),
+	font_rules = {
+		{
+			italic = true,
+			font = wezterm.font("PlemolJP Console NF", { italic = true }),
+		},
 	},
 }
-
-return config
