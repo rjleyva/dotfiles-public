9af300131f2a24068e5c93c3725d96a1d49d9dbb
rjleyva <rleyva@tutanota.com>
2025-05-21 03:32:27 -0400

    feat(nvim): add blink.cmp to enhanced autocompletion

---
+++ nvim/lua/leyva/plugins/blink.lua
@@ -0,0 +1,43 @@
+return {
+  "saghen/blink.cmp",
+  version = "*",
+  lazy = true,
+  enabled = true,
+  event = { "InsertEnter", "CmdlineEnter" },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    {
+      "saghen/blink.compat",
+      optional = true,
+      version = vim.g.lazyvim_blink_main and "*" or "1.*",
+    },
+  },
+  opts = {
+    keymap = {
+      preset = "default",
+    },
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    signature = {
+      enabled = true,
+    },
+    fuzzy = {
+      implementation = "prefer_rust_with_warning",
+    },
+    cmdline = {
+      keymap = { preset = "inherit" },
+      completion = {
+        menu = {
+          auto_show = false,
+        },
+        ghost_text = {
+          enabled = false,
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("blink.cmp").setup(opts)
+  end,
+}
