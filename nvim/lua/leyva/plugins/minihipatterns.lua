395a112bc70ed05fe8fef7a520bd32aadb50295d
rjleyva <rleyva@tutanota.com>
2025-05-21 03:44:12 -0400

    feat(mini-hipatterns): add pattern-based highlighting for FIXME, TODO, HACK, and NOTE

---
+++ nvim/lua/leyva/plugins/minihipatterns.lua
@@ -0,0 +1,22 @@
+return {
+  "echasnovski/mini.hipatterns",
+  version = "*",
+  config = function()
+    local hipatterns = require("mini.hipatterns")
+
+    hipatterns.setup({
+      highlighters = {
+        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+        hex_color = hipatterns.gen_highlighter.hex_color(),
+      },
+      delay = {
+        text_change = 200,
+        scroll = 50,
+      },
+    })
+  end,
+}
