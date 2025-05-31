c3dacb4e414d6d371bc1dc15eced467d4116ef82
rjleyva <rleyva@tutanota.com>
2025-05-31 13:39:37 -0400

    style(rose-pine): set extend_background_behind_borders to false

--- nvim/lua/leyva/plugins/colorscheme.lua
+++ nvim/lua/leyva/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
       variant = "main", -- auto, main, moon, or dawn
       dark_variant = "main", -- main, moon, or dawn
       dim_inactive_windows = false,
-      extend_background_behind_borders = true,
+      extend_background_behind_borders = false,
 
       enable = {
         terminal = true,
