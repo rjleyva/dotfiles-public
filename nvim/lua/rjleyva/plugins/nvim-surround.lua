6eb6a421126cb0477b52146312cbed35b458ef92
RJ <rleyva@tutanota.com>
2025-06-03 18:59:34 -0400

    feat(plugins): add nvim-surround.nvim for text-surroundings manipulation

---
+++ nvim/lua/rjleyva/plugins/nvim-surround.lua
@@ -0,0 +1,16 @@
+return {
+  "kylechui/nvim-surround",
+  event = "VeryLazy",
+  opts = {
+    keymaps = {
+      normal = "ys",
+      normal_cur = "yss",
+      normal_line = "yS",
+      normal_cur_line = "ySS",
+      visual = "S",
+      visual_line = "gS",
+      delete = "ds",
+      change = "cs",
+    },
+  },
+}
