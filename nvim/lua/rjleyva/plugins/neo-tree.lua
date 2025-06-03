89660aaf08c6ff8609e2e5b63d9b2937c05268ab
RJ <rleyva@tutanota.com>
2025-06-03 19:27:05 -0400

    feat(plugins): add neo-tree.nvim for file tree explorer support

---
+++ nvim/lua/rjleyva/plugins/neo-tree.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false,
+  keys = {
+      {
+      "<leader>ee",
+      "<cmd>Neotree toggle<cr>",
+      desc = "Toggle Neo-tree",
+    },
+  },
+  opts = {
+  },
+}
