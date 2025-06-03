b2dcb25c17a00bcfc89c65fb87ec2e7fbef4fabe
RJ <rleyva@tutanota.com>
2025-06-03 18:21:42 -0400

    feat(plugins): add which-key for better keybinding discovery

---
+++ nvim/lua/rjleyva/plugins/which-key.lua
@@ -0,0 +1,39 @@
+return {
+  "folke/which-key.nvim",
+  version = "*",
+  event = "VeryLazy",
+  opts = {
+    plugins = {
+      spelling = { enabled = false },
+      presets = {
+        operators = true,
+        motions = true,
+        text_objects = true,
+      },
+    },
+    win = {
+      border = "rounded",
+    },
+    layout = {
+      align = "center",
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 500
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps",
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    wk.add({
+      {"<leader>c", group = "+code" },
+    })
+  end,
+}
