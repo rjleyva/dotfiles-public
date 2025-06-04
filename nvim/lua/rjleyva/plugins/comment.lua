1818dc988c2960dbc0b076466e1e61e816caa528
RJ <rleyva@tutanota.com>
2025-06-04 16:15:11 -0400

    feat(plugins): add Comment.nvim with ts-context-commentstring integration

---
+++ nvim/lua/rjleyva/plugins/comment.lua
@@ -0,0 +1,13 @@
+return {
+  "numToStr/Comment.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+  opts = function()
+    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+    return {
+      pre_hook = ts_context_commentstring.create_pre_hook(),
+    }
+  end,
+}
