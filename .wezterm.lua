0782380466912e6c397f9d7add33dae860d38fc3
RJ <rleyva@tutanota.com>
2025-06-04 13:10:08 -0400

    chore(wezterm): add wezterm terminal configuration

---
+++ .wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require("wezterm")
+
+return {
+	window_decorations = "RESIZE",
+	enable_tab_bar = false,
+	window_background_opacity = 0.75,
+	macos_window_background_blur = 10,
+
+	color_scheme = "Solarized Dark - Patched",
+	max_fps = 60,
+
+	font = wezterm.font("PlemolJP Console NF"),
+	font_size = 18,
+
+	font_rules = {
+		{
+			italic = true,
+			font = wezterm.font("PlemolJP Console NF", { italic = true }),
+		},
+	},
+}
