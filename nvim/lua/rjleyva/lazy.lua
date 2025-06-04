13dd6a2e344139c57ea989b8dfdde1870ad67b4d
RJ <rleyva@tutanota.com>
2025-06-04 05:32:35 -0400

    chore(lazy): disable change_detection notifications

--- nvim/lua/rjleyva/lazy.lua
+++ nvim/lua/rjleyva/lazy.lua
@@ -11,9 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  spec = {
-    { import = "rjleyva.plugins" },
+require("lazy").setup({ { import = "rjleyva.plugins" }, { import = "rjleyva.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
   },
-  checker = { enabled = true },
+  change_detection = {
+    notify = false,
+  },
 })
