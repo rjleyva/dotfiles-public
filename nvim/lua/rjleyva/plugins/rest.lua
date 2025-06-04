f559a4c95b5228bf1efa126de01e6a3d593a4dca
RJ <rleyva@tutanota.com>
2025-06-04 05:59:43 -0400

    refactor(rest): clean up config and switch to opts table

--- nvim/lua/rjleyva/plugins/rest.lua
+++ nvim/lua/rjleyva/plugins/rest.lua
@@ -11,68 +11,68 @@ return {
         http = "http",
       },
     })
-
-    vim.g.rest_nvim = {
-      custom_dynamic_variables = {},
-      request = {
-        skip_ssl_verification = true,
-        hooks = {
-          encode_url = true,
-          user_agent = "rest.nvim",
-          set_content_type = true,
-        },
-      },
-      response = {
-        hooks = {
-          decode_url = true,
-          format = true,
-        },
-      },
-      clients = {
-        curl = {
-          statistics = {
-            { id = "time_total", winbar = "take", title = "Time taken" },
-            { id = "size_download", winbar = "size", title = "Download size" },
-          },
-          opts = {
-            set_compressed = false,
-            certificates = {},
-          },
-        },
-      },
-      cookies = {
-        enable = true,
-        path = vim.fs.joinpath(vim.fn.stdpath("data"), "rest-nvim.cookies"),
-      },
-      env = {
-        enable = true,
-        pattern = ".*%.env.*",
-        find = function()
-          local config = require("rest-nvim.config")
-          return vim.fs.find(function(name, _)
-            return name:match(config.env.pattern)
-          end, {
-            path = vim.fn.getcwd(),
-            type = "file",
-            limit = math.huge,
-          })
-        end,
-      },
-      ui = {
-        winbar = true,
-        keybinds = {
-          prev = "H",
-          next = "L",
-        },
-      },
-      highlight = {
-        enable = true,
-        timeout = 750,
-      },
-      _log_level = vim.log.levels.WARN,
-    }
   end,
-  config = function()
-    vim.keymap.set("n", "\\r", "<cmd>Rest run<cr>", { desc = "Run HTTP request" })
+  opts = {
+    custom_dynamic_variables = {},
+    request = {
+      skip_ssl_verification = true,
+      hooks = {
+        encode_url = true,
+        user_agent = "rest.nvim",
+        set_content_type = true,
+      },
+    },
+    response = {
+      hooks = {
+        decode_url = true,
+        format = true,
+      },
+    },
+    clients = {
+      curl = {
+        statistics = {
+          { id = "time_total", title = "Time taken" },
+          { id = "size_download", title = "Download size" },
+        },
+        opts = {
+          set_compressed = false,
+          certificates = {},
+        },
+      },
+    },
+    cookies = {
+      enable = true,
+      path = vim.fs.joinpath(vim.fn.stdpath("data"), "rest-nvim.cookies"),
+    },
+    env = {
+      enable = true,
+      pattern = ".*%.env.*",
+      find = function()
+        local config = require("rest-nvim.config")
+        return vim.fs.find(function(name, _)
+          return name:match(config.env.pattern)
+        end, {
+          path = vim.fn.getcwd(),
+          type = "file",
+          limit = math.huge,
+        })
+      end,
+    },
+    ui = {
+      winbar = true,
+      keybinds = {
+        prev = "H",
+        next = "L",
+      },
+    },
+    highlight = {
+      enable = true,
+      timeout = 750,
+    },
+    _log_level = vim.log.levels.WARN,
+  },
+  config = function(_, opts)
+    require("rest-nvim").setup(opts)
+    vim.keymap.set("n", "\\r", "<cmd>Rest run<CR>", { desc = "Run HTTP request" })
   end,
 }
