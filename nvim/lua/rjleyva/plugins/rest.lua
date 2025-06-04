cf344acd3db063fee64db511bb5a1e1053a40a3f
RJ <rleyva@tutanota.com>
2025-06-04 03:50:56 -0400

    feat(plugins): add rest.nvim for HTTP request support

---
+++ nvim/lua/rjleyva/plugins/rest.lua
@@ -0,0 +1,78 @@
+return {
+  "rest-nvim/rest.nvim",
+  ft = { "http" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  init = function()
+    vim.filetype.add({
+      extension = {
+        http = "http",
+      },
+    })
+
+    vim.g.rest_nvim = {
+      custom_dynamic_variables = {},
+      request = {
+        skip_ssl_verification = true,
+        hooks = {
+          encode_url = true,
+          user_agent = "rest.nvim",
+          set_content_type = true,
+        },
+      },
+      response = {
+        hooks = {
+          decode_url = true,
+          format = true,
+        },
+      },
+      clients = {
+        curl = {
+          statistics = {
+            { id = "time_total", winbar = "take", title = "Time taken" },
+            { id = "size_download", winbar = "size", title = "Download size" },
+          },
+          opts = {
+            set_compressed = false,
+            certificates = {},
+          },
+        },
+      },
+      cookies = {
+        enable = true,
+        path = vim.fs.joinpath(vim.fn.stdpath("data"), "rest-nvim.cookies"),
+      },
+      env = {
+        enable = true,
+        pattern = ".*%.env.*",
+        find = function()
+          local config = require("rest-nvim.config")
+          return vim.fs.find(function(name, _)
+            return name:match(config.env.pattern)
+          end, {
+            path = vim.fn.getcwd(),
+            type = "file",
+            limit = math.huge,
+          })
+        end,
+      },
+      ui = {
+        winbar = true,
+        keybinds = {
+          prev = "H",
+          next = "L",
+        },
+      },
+      highlight = {
+        enable = true,
+        timeout = 750,
+      },
+      _log_level = vim.log.levels.WARN,
+    }
+  end,
+  config = function()
+    vim.keymap.set("n", "\\r", "<cmd>Rest run<cr>", { desc = "Run HTTP request" })
+  end,
+}
