b56090e1b33016b54457dfc33688d633b4a0ff74
RJ <rleyva@tutanota.com>
2025-06-04 12:09:01 -0400

    feat(plugins): add nvim-lint for asynchronous linting

---
+++ nvim/lua/rjleyva/plugins/linting.lua
@@ -0,0 +1,43 @@
+return {
+  "mfussenegger/nvim-lint",
+  version = "*",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+    },
+  },
+  config = function(_, opts)
+    local lint = require("lint")
+    lint.linters_by_ft = opts.linters_by_ft
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      desc = "Run linters on buffer events",
+      callback = function()
+        local ft = vim.bo.filetype
+        if lint.linters_by_ft[ft] ~= nil then
+          lint.try_lint()
+        end
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>ll", function()
+      local ft = vim.bo.filetype
+      if lint.linters_by_ft[ft] ~= nil then
+        lint.try_lint()
+      else
+        vim.notify("No linter configured for this filetype: " .. ft, vim.log.levels.WARN)
+      end
+    end, { desc = "Trigger linting for current file" })
+
+    vim.keymap.set("n", "<leader>le", function()
+      vim.diagnostic.setqflist()
+    end, { desc = "Send lint diagnostics to quickfix list" })
+  end,
+}
