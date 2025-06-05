7b1f07286b65bacda3f762b38f37caab79dbb272
RJ <rleyva@tutanota.com>
2025-06-05 09:58:34 -0400

    chore(fzf): remap and keys definitions

--- nvim/lua/rjleyva/plugins/fzf.lua
+++ nvim/lua/rjleyva/plugins/fzf.lua
@@ -7,144 +7,123 @@ return {
   },
   keys = {
     {
-      "<leader>fc",
+      "<leader>fs",
       function()
         require("fzf-lua").commands()
       end,
-      desc = "Search commands",
-    },
-    {
-      "<leader>fC",
-      function()
-        require("fzf-lua").command_history()
-      end,
-      desc = "Search command history",
+      desc = "Search Neovim commands",
     },
     {
       "<leader>ff",
       function()
         require("fzf-lua").files()
       end,
-      desc = "Search files",
+      desc = "Find files",
     },
     {
       "<leader>fh",
       function()
         require("fzf-lua").highlights()
       end,
-      desc = "Search highlights",
+      desc = "Search highlights groups",
     },
     {
       "<leader>fm",
       function()
         require("fzf-lua").marks()
       end,
-      desc = "Search marks",
+      desc = "Search marks in buffer",
     },
     {
       "<leader>fk",
       function()
         require("fzf-lua").keymaps()
       end,
-      desc = "Search keymaps",
+      desc = "Search defined keymaps",
     },
     {
       "<leader>fl",
       function()
         require("fzf-lua").live_grep()
       end,
-      desc = "Live grep",
+      desc = "Live grep across files",
     },
     {
-      "<leader>fgf",
+      "<leader>fg",
       function()
         require("fzf-lua").git_files()
       end,
-      desc = "Search git files",
+      desc = "Search tracked Git files",
     },
     {
-      "<leader>fgb",
+      "<leader>fb",
       function()
         require("fzf-lua").git_branches()
       end,
-      desc = "Search git branches",
+      desc = "Search Git branches",
     },
     {
-      "<leader>fgc",
+      "<leader>fc",
       function()
         require("fzf-lua").git_commits()
       end,
-      desc = "Search git commits",
+      desc = "Search Git commits",
     },
     {
-      "<leader>fgC",
+      "<leader>fB",
       function()
         require("fzf-lua").git_bcommits()
       end,
-      desc = "Search buffer git commits",
+      desc = "Search buffer Git commits",
     },
     {
       "<leader>fr",
       function()
         require("fzf-lua").resume()
       end,
-      desc = "Resume previous search",
-    },
-    {
-      "<leader>fb",
-      function()
-        require("fzf-lua").dap_breakpoints()
-      end,
-      desc = "FZF: DAP Breakpoints",
-    },
-    {
-      "<leader>fv",
-      function()
-        require("fzf-lua").dap_variables()
-      end,
-      desc = "FZF: DAP Variables",
-    },
-    {
-      "<leader>fs",
-      function()
-        require("fzf-lua").dap_frames()
-      end,
-      desc = "FZF: DAP Stack Frames",
+      desc = "Resume last search session",
     },
     {
       "<leader>fd",
       function()
         vim.cmd("FzfDirectories")
       end,
-      desc = "Search directories",
+      desc = "Search project directories",
     },
     {
       "<leader>fR",
       function()
         require("fzf-lua").lsp_references()
       end,
-      desc = "References",
+      desc = "References (LSP)",
     },
     {
       "<leader>fD",
       function()
         require("fzf-lua").lsp_definitions()
       end,
-      desc = "Definitions",
+      desc = "Definitions (LSP)",
     },
     {
       "<leader>fi",
       function()
         require("fzf-lua").lsp_implementations()
       end,
-      desc = "Implementations",
+      desc = "Implementations (LSP)",
     },
     {
       "<leader>ft",
       function()
         require("fzf-lua").lsp_typedefs()
       end,
-      desc = "Type Definitions",
+      desc = "Type Definitions (LSP)",
+    },
+    {
+      "<leader>fN",
+      function()
+        require("fzf-lua").lsp_document_diagnostics()
+      end,
+      desc = "Document Diagnostics (LSP)",
     },
   },
   opts = {
