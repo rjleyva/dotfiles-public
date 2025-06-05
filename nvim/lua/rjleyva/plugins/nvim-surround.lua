dbe4fae4de5508658ff857951d0707b2d9b315fa
RJ <rleyva@tutanota.com>
2025-06-05 06:56:42 -0400

    chore(nvim-surround): define keymaps using lazy.nvim keys field
    - Add description

--- nvim/lua/rjleyva/plugins/nvim-surround.lua
+++ nvim/lua/rjleyva/plugins/nvim-surround.lua
@@ -1,17 +1,30 @@
 return {
-  version = "v3.1.2",
   "kylechui/nvim-surround",
+  version = "v3.1.2",
   event = "VeryLazy",
-  opts = {
-    keymaps = {
-      normal = "ys",
-      normal_cur = "yss",
-      normal_line = "yS",
-      normal_cur_line = "ySS",
-      visual = "S",
-      visual_line = "gS",
-      delete = "ds",
-      change = "cs",
-    },
+  opts = {},
+  keys = {
+    { "sa", desc = "Add surrounding", mode = "n" },
+    { "sy", desc = "Add surrounding to current word", mode = "n" },
+    { "sl", desc = "Add surrounding to current line", mode = "n" },
+    { "sY", desc = "Add surrounding to entire line", mode = "n" },
+    { "s", desc = "Add surrounding in visual mode", mode = "v" },
+    { "sg", desc = "Add surrounding to visual lines", mode = "v" },
+    { "sd", desc = "Delete surrounding", mode = "n" },
+    { "sc", desc = "Change surrounding", mode = "n" },
   },
+  config = function(_, _)
+    require("nvim-surround").setup({
+      keymaps = {
+        normal = "sa",
+        normal_cur = "sy",
+        normal_line = "sl",
+        normal_cur_line = "sY",
+        visual = "s",
+        visual_line = "sg",
+        delete = "sd",
+        change = "sc",
+      },
+    })
+  end,
 }
