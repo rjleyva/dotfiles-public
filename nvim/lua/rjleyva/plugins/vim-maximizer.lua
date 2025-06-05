256d96a31584dde9158f5cbfa2c2a4b28d9208a9
RJ <rleyva@tutanota.com>
2025-06-05 04:49:10 -0400

    feat(vim-maximizer): remap <leader>ms to <leader>ts to toggle split maximization

--- nvim/lua/rjleyva/plugins/vim-maximizer.lua
+++ nvim/lua/rjleyva/plugins/vim-maximizer.lua
@@ -2,6 +2,6 @@ return {
   "szw/vim-maximizer",
   event = "VeryLazy",
   keys = {
-    { "<leader>ms", "<cmd>MaximizerToggle<CR>", desc = "Toggle Split Maximization" },
+    { "<leader>ts", "<cmd>MaximizerToggle<CR>", desc = "Toggle Split Maximization" },
   },
 }
