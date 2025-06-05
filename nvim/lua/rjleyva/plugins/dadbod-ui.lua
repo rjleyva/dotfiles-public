78e4d98ab49994fef92aad1c98b954f77ac18947
RJ <rleyva@tutanota.com>
2025-06-05 03:20:33 -0400

    chore(dadbod): make keys description more descriptive

--- nvim/lua/rjleyva/plugins/dadbod-ui.lua
+++ nvim/lua/rjleyva/plugins/dadbod-ui.lua
@@ -10,10 +10,10 @@ return {
     },
   },
   keys = {
-    { "<leader>db", "<cmd>DBUI<cr>", desc = "Open DBUI" },
-    { "<leader>dt", "<cmd>DBUIToggle<cr>", desc = "Toggle DBUI" },
-    { "<leader>da", "<cmd>DBUIAddConnection<cr>", desc = "Add DB connection" },
-    { "<leader>df", "<cmd>DBUIFindBuffer<cr>", desc = "Find DB buffer" },
+    { "<leader>dl", "<cmd>DBUI<cr>", desc = "Launch Database UI Explorer" },
+    { "<leader>dt", "<cmd>DBUIToggle<cr>", desc = "Toggle visibility of Database UI" },
+    { "<leader>da", "<cmd>DBUIAddConnection<cr>", desc = "Add a new database connection" },
+    { "<leader>ds", "<cmd>DBUIFindBuffer<cr>", desc = "Search and open an existing DBUI buffer" },
   },
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
