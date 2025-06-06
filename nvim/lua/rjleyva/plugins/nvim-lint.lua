c37d781d53237e4c352ca929d0ada0c9bd08d352
RJ <rleyva@tutanota.com>
2025-06-06 04:18:46 -0400

    chore(nvim-lint): switch lua linter to selene, and remap to <leader>lL

--- nvim/lua/rjleyva/plugins/nvim-lint.lua
+++ nvim/lua/rjleyva/plugins/nvim-lint.lua
@@ -4,7 +4,7 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   opts = {
     linters_by_ft = {
-      lua = { "luacheck" },
+      lua = { "selene" },
       javascript = { "eslint_d" },
       typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
@@ -15,7 +15,6 @@ return {
     local lint = require("lint")
     lint.linters_by_ft = opts.linters_by_ft
 
-    -- luacheck: globals vim
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
@@ -29,7 +28,7 @@ return {
       end,
     })
 
-    vim.keymap.set("n", "<leader>ll", function()
+    vim.keymap.set("n", "<leader>lL", function()
       local ft = vim.bo.filetype
       if lint.linters_by_ft[ft] ~= nil then
         lint.try_lint()
