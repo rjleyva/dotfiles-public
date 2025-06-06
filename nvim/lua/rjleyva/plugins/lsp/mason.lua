08818c97875a817f227a34059ed6960436c3ceab
RJ <rleyva@tutanota.com>
2025-06-06 13:55:30 -0400

    refactor(mason): migrate to lazy.nvim opts style

--- nvim/lua/rjleyva/plugins/lsp/mason.lua
+++ nvim/lua/rjleyva/plugins/lsp/mason.lua
@@ -1,131 +1,47 @@
 return {
   "williamboman/mason.nvim",
-  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "jay-babu/mason-null-ls.nvim",
-    "nvimtools/none-ls.nvim",
   },
-  opts = function()
-    return {
-      mason = {
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-            check_outdated_packages_on_open = true,
-          },
-        },
-        log_level = vim.log.levels.WARN,
+  opts = {
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
       },
-      mason_lspconfig = {
-        ensure_installed = {
-          "astro",
-          "cssls",
-          "emmet_ls",
-          "graphql",
-          "html",
-          "jsonls",
-          "lua_ls",
-          "prismals",
-          "svelte",
-          "ts_ls",
-          "vtsls",
-        },
-        automatic_installation = true,
-        automatic_enable = false,
-      },
-      mason_tool_installer = {
-        ensure_installed = {
-          "eslint_d",
-          "prettier",
-          "selene",
-          "stylua",
-        },
-        automatic_installation = true,
-        automatic_enable = true,
-        run_on_start = true,
-        start_delay = 1000,
-      },
-      mason_null_ls = {
-        ensure_installed = { "selene", "eslint_d", "prettier", "stylua" },
-        automatic_installation = true,
-        handlers = {},
-      },
-    }
-  end,
+    },
+  },
   config = function(_, opts)
-    require("mason").setup(opts.mason)
-    require("mason-lspconfig").setup(opts.mason_lspconfig)
-    require("mason-tool-installer").setup(opts.mason_tool_installer)
-    require("mason-null-ls").setup(opts.mason_null_ls)
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
 
-    local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
-    if not string.find(vim.env.PATH, mason_bin, 1, true) then
-      vim.env.PATH = vim.env.PATH .. ":" .. mason_bin
-    end
+    mason.setup(opts)
 
-    local null_ls = require("null-ls")
-    local util = require("lspconfig.util")
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "astro",
+        "ts_ls",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "svelte",
+        "lua_ls",
+        "graphql",
+        "emmet_ls",
+        "vtsls",
+      },
+    })
 
-    local eslint_d = {
-      method = null_ls.methods.DIAGNOSTICS,
-      filetypes = { "javascript", "typescript", "typescriptreact", "javascriptreact" },
-      generator = null_ls.generator({
-        command = "eslint_d",
-        args = { "--format", "json", "--stdin", "--stdin-filename", "$FILENAME" },
-        to_stdin = true,
-        from_stderr = false,
-        format = "json",
-        check_exit_code = function(code)
-          return code <= 1
-        end,
-        on_output = function(params)
-          local diagnostics = {}
-          local output = params.output and params.output[1] or {}
-
-          if output.messages then
-            for _, message in ipairs(output.messages) do
-              table.insert(diagnostics, {
-                row = message.line,
-                col = message.column,
-                end_row = message.endLine or message.line,
-                end_col = message.endColumn or message.column,
-                message = message.message,
-                source = "eslint_d",
-                severity = ({
-                  [2] = vim.diagnostic.severity.ERROR,
-                  [1] = vim.diagnostic.severity.WARN,
-                })[message.severity] or vim.diagnostic.severity.INFO,
-              })
-            end
-          end
-          return diagnostics
-        end,
-      }),
-    }
-
-    local sources = {
-      null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier,
-    }
-
-    local root = util.root_pattern(".eslintrc.js", ".eslintrc.json", "eslint.config.js", "package.json", ".git")(
-      vim.fn.expand("%:p")
-    ) or vim.fn.getcwd()
-
-    if
-      vim.fn.filereadable(root .. "/.eslintrc.js") == 1
-      or vim.fn.filereadable(root .. "/.eslintrc.json") == 1
-      or vim.fn.filereadable(root .. "/eslint.config.js") == 1
-    then
-      table.insert(sources, eslint_d)
-    end
-
-    vim.defer_fn(function()
-      null_ls.setup({ sources = sources })
-    end, 100)
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "selene",
+        "prettier",
+        "stylua",
+        "eslint_d",
+      },
+    })
   end,
 }
