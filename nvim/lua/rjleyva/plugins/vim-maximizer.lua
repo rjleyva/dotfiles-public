30756d7941f83d3c01dd1641276b218295111bea
RJ <rleyva@tutanota.com>
2025-06-07 10:52:34 -0400

    perf(vim-maximizer): lazy-load vim-maximizer on <leader>ts keypress

--- nvim/lua/rjleyva/plugins/vim-maximizer.lua
+++ nvim/lua/rjleyva/plugins/vim-maximizer.lua
@@ -1,7 +1,9 @@
 return {
   "szw/vim-maximizer",
-  event = "VeryLazy",
   keys = {
     { "<leader>ts", "<cmd>MaximizerToggle<CR>", desc = "Toggle Split Maximization" },
   },
+  init = function()
+    vim.g.maximizer_set_default_mapping = 0
+  end,
 }
