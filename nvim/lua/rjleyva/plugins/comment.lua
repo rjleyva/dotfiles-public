fce25e85475e3f7bb6d08ecabc0a08c56e6fc9cd
RJ <rleyva@tutanota.com>
2025-06-07 13:09:14 -0400

    perf(comment): lazy-load nvim-ts-context-commentstring on demand

--- nvim/lua/rjleyva/plugins/comment.lua
+++ nvim/lua/rjleyva/plugins/comment.lua
@@ -2,11 +2,20 @@ return {
   "numToStr/Comment.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
+    {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      lazy = true,
+    },
   },
   opts = {
     pre_hook = function(...)
-      local ok, ts_context = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+      local ok, ts_context = pcall(function()
+        if not package.loaded["ts_context_commentstring"] then
+          require("lazy").load({ plugins = { "nvim-ts-context-commentstring" } })
+        end
+        return require("ts_context_commentstring.integrations.comment_nvim")
+      end)
+
       if ok then
         return ts_context.create_pre_hook()(...)
       end
