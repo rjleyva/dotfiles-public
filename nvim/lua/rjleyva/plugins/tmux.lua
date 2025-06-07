99054fff1120e78c67bc95a8b084682643c0f11b
RJ <rleyva@tutanota.com>
2025-06-07 10:44:12 -0400

    perf(vim-tmux): lazy-load vim-tmux-navigator on keypress

--- nvim/lua/rjleyva/plugins/tmux.lua
+++ nvim/lua/rjleyva/plugins/tmux.lua
@@ -1,3 +1,12 @@
 return {
   "christoomey/vim-tmux-navigator",
+  keys = {
+    { "<C-h>", desc = "Navigate to left tmux pane" },
+    { "<C-j>", desc = "Navigate to down tmux pane" },
+    { "<C-k>", desc = "Navigate to up tmux pane" },
+    { "<C-l>", desc = "Navigate to right tmux pane" },
+  },
+  config = function()
+    vim.g.tmux_navigator_no_mappings = 1
+  end,
 }
