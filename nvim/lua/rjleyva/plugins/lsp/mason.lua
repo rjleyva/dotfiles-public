4301c225cd167a4914a5edf9f9dfbbfcd5b238d2
RJ <rleyva@tutanota.com>
2025-06-07 11:39:54 -0400

    refactor(mason): restructure config with modular opts

--- nvim/lua/rjleyva/plugins/lsp/mason.lua
+++ nvim/lua/rjleyva/plugins/lsp/mason.lua
@@ -1,50 +1,61 @@
 return {
   "williamboman/mason.nvim",
+  version = "*",
   after = "neodev.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "folke/neodev.nvim",
   },
-  opts = {
-    ui = {
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗",
+  opts = function()
+    return {
+      mason = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+            check_outdated_packages_on_open = false,
+          },
+          border = "none",
+        },
+        log_level = vim.log.levels.WARN,
       },
-    },
-  },
+      mason_lspconfig = {
+        ensure_installed = {
+          "astro",
+          "ts_ls",
+          "html",
+          "cssls",
+          "tailwindcss",
+          "svelte",
+          "lua_ls",
+          "graphql",
+          "jsonls",
+          "emmet_ls",
+          "vtsls",
+        },
+        automatic_installation = true,
+      },
+      mason_tool_installer = {
+        ensure_installed = {
+          "selene",
+          "prettier",
+          "stylua",
+          "eslint_d",
+        },
+        automatic_installation = true,
+      },
+    }
+  end,
   config = function(_, opts)
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
     local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup(opts)
-
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "astro",
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "jsonls",
-        "emmet_ls",
-        "vtsls",
-      },
-    })
-
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "selene",
-        "prettier",
-        "stylua",
-        "eslint_d",
-      },
-    })
+    mason.setup(opts.mason)
+    mason_lspconfig.setup(opts.mason_lspconfig)
+    mason_tool_installer.setup(opts.mason_tool_installer)
   end,
 }
