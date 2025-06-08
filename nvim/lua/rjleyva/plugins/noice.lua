872c8dc1030068003bb61fd5eb8408e99e875bbd
RJ <rleyva@tutanota.com>
2025-06-08 02:20:43 -0400

    refactor(noice): migrate to modular opts function pattern

---
+++ nvim/lua/rjleyva/plugins/noice.lua
@@ -0,0 +1,112 @@
+return {
+  "folke/noice.nvim",
+  version = "*",
+  event = "VeryLazy",
+  dependencies = {
+    "ibhagwan/fzf-lua",
+  },
+
+  opts = function()
+    return {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+      },
+
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        lsp_doc_border = true,
+      },
+    }
+  end,
+
+  config = function(_, opts)
+    require("noice").setup(opts)
+  end,
+
+  keys = {
+    { "<leader>nn", "", desc = "+noice" },
+    {
+      "<S-Enter>",
+      function()
+        require("noice").redirect(vim.fn.getcmdline())
+      end,
+      mode = "c",
+      desc = "Redirect Cmdline",
+    },
+    {
+      "<leader>nl",
+      function()
+        require("noice").cmd("last")
+      end,
+      desc = "Noice Last Message",
+    },
+    {
+      "<leader>nh",
+      function()
+        require("noice").cmd("history")
+      end,
+      desc = "Noice History",
+    },
+    {
+      "<leader>na",
+      function()
+        require("noice").cmd("all")
+      end,
+      desc = "Noice All",
+    },
+    {
+      "<leader>nd",
+      function()
+        require("noice").cmd("dismiss")
+      end,
+      desc = "Dismiss All",
+    },
+    {
+      "<leader>np",
+      function()
+        require("noice").cmd("pick")
+      end,
+      desc = "Noice Picker (FzfLua)",
+    },
+    {
+      "<C-f>",
+      function()
+        return require("noice.lsp").scroll(4) or "<C-f>"
+      end,
+      mode = { "i", "n", "s" },
+      expr = true,
+      silent = true,
+      desc = "Scroll Forward",
+    },
+    {
+      "<C-b>",
+      function()
+        return require("noice.lsp").scroll(-4) or "<C-b>"
+      end,
+      mode = { "i", "n", "s" },
+      expr = true,
+      silent = true,
+      desc = "Scroll Backward",
+    },
+  },
+}
