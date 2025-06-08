a4bff46043a583524cf8a5b88e7692dcb649fb0d
RJ <rleyva@tutanota.com>
2025-06-08 11:54:11 -0400

    perf(alpha): optimize alpha.nvim loading with lazy opts and conditional enable

---
+++ nvim/lua/rjleyva/plugins/alpha.lua
@@ -0,0 +1,33 @@
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  enabled = (vim.fn.argc(-1) == 0),
+  opts = function()
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
+      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>FzfLua files<CR>"),
+      dashboard.button("SPC fs", "  > Find Word", "<cmd>FzfLua live_grep<CR>"),
+      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+    }
+
+    return dashboard.config
+  end,
+  config = function(_, opts)
+    require("alpha").setup(opts)
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
+}
