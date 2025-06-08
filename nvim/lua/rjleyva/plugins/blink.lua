ef99624d5421b096213659b0c616acb92100dab1
RJ <rleyva@tutanota.com>
2025-06-08 02:44:57 -0400

    perf(blink): defer setup to InsertEnter and CmdlineEnter for faster startup

--- nvim/lua/rjleyva/plugins/blink.lua
+++ nvim/lua/rjleyva/plugins/blink.lua
@@ -1,42 +1,33 @@
 return {
   "saghen/blink.cmp",
   version = "1.*",
-  enabled = true,
   event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
-    "rafamadriz/friendly-snippets",
+    { "rafamadriz/friendly-snippets", event = "InsertEnter" },
     {
       "saghen/blink.compat",
       optional = true,
       version = vim.g.lazyvim_blink_main and "*" or "1.*",
     },
   },
-  opts = {
-    keymap = {
-      preset = "default",
-    },
-    appearance = {
-      nerd_font_variant = "mono",
-    },
-    signature = {
-      enabled = true,
-    },
-    fuzzy = {
-      implementation = "prefer_rust_with_warning",
-    },
-    cmdline = {
-      keymap = { preset = "inherit" },
-      completion = {
-        menu = {
-          auto_show = false,
-        },
-        ghost_text = {
-          enabled = false,
+  opts = function()
+    return {
+      keymap = { preset = "default" },
+      appearance = { nerd_font_variant = "mono" },
+      signature = { enabled = true },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+      cmdline = {
+        keymap = { preset = "inherit" },
+        completion = {
+          menu = { auto_show = false },
+          ghost_text = { enabled = false },
         },
       },
-    },
-  },
+    }
+  end,
   config = function(_, opts)
-    require("blink.cmp").setup(opts)
+    vim.schedule(function()
+      require("blink.cmp").setup(opts)
+    end)
   end,
 }
