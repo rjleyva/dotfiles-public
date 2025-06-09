f9b53f682b86873890e8a9124e6950fa8039e03d
RJ <rleyva@tutanota.com>
2025-06-09 11:58:44 -0400

    chore(keymaps): remove inline comments for cleaner plugin separation

--- nvim/lua/rjleyva/core/keymaps.lua
+++ nvim/lua/rjleyva/core/keymaps.lua
@@ -1,25 +1,19 @@
 local keymap = vim.keymap
 
--- Exit insert mode by typing 'jk'
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
--- Clear search highlights
 keymap.set("n", "<leader>ch", ":nohl<CR>", { desc = "Clear search highlights" })
 
--- Delete a character without copying it to the register
 keymap.set("n", "x", '"_x')
 
--- Increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
--- Window management
 keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>wx", "<cmd>close<CR>", { desc = "Close current split" })
 
--- Tab management
 keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 keymap.set("n", "<leader>t]", "<cmd>tabn<CR>", { desc = "Go to next tab" })
