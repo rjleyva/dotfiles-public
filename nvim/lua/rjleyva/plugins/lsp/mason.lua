fe32afbeed2e57a98f6aeeed4247dc126fef7d08
RJ <rleyva@tutanota.com>
2025-06-10 19:04:59 -0400

    perf(mason): restructure config to use opts and lazy load after neodev

--- nvim/lua/rjleyva/plugins/lsp/mason.lua
+++ nvim/lua/rjleyva/plugins/lsp/mason.lua
@@ -1,61 +1,52 @@
 return {
   "williamboman/mason.nvim",
-  version = "*",
-  after = "neodev.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
+    "folke/neodev.nvim",
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "folke/neodev.nvim",
   },
-  opts = function()
-    return {
-      mason = {
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-            check_outdated_packages_on_open = false,
-          },
-          border = "none",
-        },
-        log_level = vim.log.levels.WARN,
+  opts = {
+    ui = {
+      border = "none",
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
       },
-      mason_lspconfig = {
-        ensure_installed = {
-          "astro",
-          "svelte",
-          "ts_ls",
-          "html",
-          "cssls",
-          "tailwindcss",
-          "lua_ls",
-          "graphql",
-          "jsonls",
-          "emmet_ls",
-          "vtsls",
-        },
-        automatic_installation = true,
+    },
+    log_level = vim.log.levels.WARN,
+    lspconfig = {
+      ensure_installed = {
+        "astro",
+        "svelte",
+        "ts_ls",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "lua_ls",
+        "graphql",
+        "jsonls",
+        "emmet_ls",
+        "vtsls",
       },
-      mason_tool_installer = {
-        ensure_installed = {
-          "selene",
-          "prettier",
-          "stylua",
-          "eslint_d",
-        },
-        automatic_installation = true,
+      automatic_installation = true,
+    },
+    tools = {
+      ensure_installed = {
+        "selene",
+        "prettier",
+        "stylua",
+        "eslint_d",
       },
-    }
-  end,
+      auto_update = false,
+      run_on_start = true,
+    },
+  },
   config = function(_, opts)
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
-
-    mason.setup(opts.mason)
-    mason_lspconfig.setup(opts.mason_lspconfig)
-    mason_tool_installer.setup(opts.mason_tool_installer)
+    require("neodev").setup({})
+    require("mason").setup(opts)
+    require("mason-lspconfig").setup(opts.lspconfig)
+    require("mason-tool-installer").setup(opts.tools)
   end,
 }
