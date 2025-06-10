430037a8588bfbca08545aabcfee5cc8511e50e8
RJ <rleyva@tutanota.com>
2025-06-10 18:54:30 -0400

    perf(surround): lazy load plugin on key usage to improve startup time

--- nvim/lua/rjleyva/plugins/nvim-surround.lua
+++ nvim/lua/rjleyva/plugins/nvim-surround.lua
@@ -1,30 +1,25 @@
 return {
   "kylechui/nvim-surround",
   version = "v3.1.2",
-  event = "InsertEnter",
   keys = {
-    { "sm", desc = "Add surrounding (manual input)", mode = "n" },
-    { "sy", desc = "Add surrounding to current word", mode = "n" },
-    { "sl", desc = "Add surrounding to current line", mode = "n" },
-    { "sY", desc = "Add surrounding to entire line", mode = "n" },
-    { "s", desc = "Add surrounding in visual mode", mode = "v" },
-    { "sg", desc = "Add surrounding to visual lines", mode = "v" },
+    { "sm", desc = "Surround (Manual)", mode = "n" },
+    { "sy", desc = "Surround word", mode = "n" },
+    { "sl", desc = "Surround line", mode = "n" },
+    { "sY", desc = "Surround full line", mode = "n" },
     { "sd", desc = "Delete surrounding", mode = "n" },
     { "sc", desc = "Change surrounding", mode = "n" },
+    { "s", desc = "Surround (Visual)", mode = "v" },
   },
-
-  config = function(_, _)
-    require("nvim-surround").setup({
-      keymaps = {
-        normal = "sm",
-        normal_cur = "sy",
-        normal_line = "sl",
-        normal_cur_line = "sY",
-        visual = "s",
-        visual_line = "sg",
-        delete = "sd",
-        change = "sc",
-      },
-    })
-  end,
+  opts = {
+    keymaps = {
+      normal = "sm",
+      normal_cur = "sy",
+      normal_line = "sl",
+      normal_cur_line = "sY",
+      visual = "s",
+      visual_line = "sg",
+      delete = "sd",
+      change = "sc",
+    },
+  },
 }
