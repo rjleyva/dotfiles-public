3de3f182cfc4035120a3a4667153e489cf4bb3c0
RJ <rleyva@tutanota.com>
2025-06-10 17:50:55 -0400

    perf(refactoring): lazy-load refactoring.nvim only for specific filetypes

--- nvim/lua/rjleyva/plugins/refactoring.lua
+++ nvim/lua/rjleyva/plugins/refactoring.lua
@@ -1,6 +1,7 @@
 return {
   "ThePrimeagen/refactoring.nvim",
   version = "*",
+  ft = { "lua", "astro", "svelte", "javascript", "typescript", "javascriptreact", "typescriptreact" },
   keys = {
     {
       "<leader>r",
