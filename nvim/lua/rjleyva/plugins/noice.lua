6960eb1afd9fe1822e6d89756986f9be415353fd
RJ <rleyva@tutanota.com>
2025-06-11 15:52:32 -0400

    feat(noice): enable detailed LSP progress notifications with notify view

--- nvim/lua/rjleyva/plugins/noice.lua
+++ nvim/lua/rjleyva/plugins/noice.lua
@@ -11,6 +11,11 @@ return {
   opts = function()
     return {
       lsp = {
+        progress = {
+          enabled = true,
+          format = "detailed",
+          view = "notify",
+        },
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
