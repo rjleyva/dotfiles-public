56aa02056452f580c018c6f58380e2154570e9cd
RJ <rleyva@tutanota.com>
2025-06-12 18:22:24 -0400

    chore(neo-tree): move opts to top of plugin spec for consistency

--- nvim/lua/rjleyva/plugins/neo-tree.lua
+++ nvim/lua/rjleyva/plugins/neo-tree.lua
@@ -2,32 +2,6 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   cmd = { "Neotree" },
-  keys = {
-    {
-      "<leader>ee",
-      "<cmd>Neotree toggle<cr>",
-      desc = "Toggle Neo-tree sidebar",
-    },
-    {
-      "<leader>ec",
-      function()
-        local reveal_file = vim.fn.expand("%:p")
-        ---@diagnostic disable-next-line: undefined-field
-        if reveal_file == "" or vim.uv.fs_stat(reveal_file) == nil then
-          reveal_file = vim.fn.getcwd()
-        end
-
-        require("neo-tree.command").execute({
-          action = "focus",
-          source = "filesystem",
-          position = "right",
-          reveal_file = reveal_file,
-          reveal_force_cwd = true,
-        })
-      end,
-      desc = "Reveal current file in Neo-tree",
-    },
-  },
   dependencies = {
     {
       "nvim-lua/plenary.nvim",
@@ -55,6 +29,33 @@ return {
       window = { position = "right" },
     },
   },
+
+  keys = {
+    {
+      "<leader>ee",
+      "<cmd>Neotree toggle<cr>",
+      desc = "Toggle Neo-tree sidebar",
+    },
+    {
+      "<leader>ec",
+      function()
+        local reveal_file = vim.fn.expand("%:p")
+        if reveal_file == "" or vim.loop.fs_stat(reveal_file) == nil then
+          reveal_file = vim.fn.getcwd()
+        end
+
+        require("neo-tree.command").execute({
+          action = "focus",
+          source = "filesystem",
+          position = "right",
+          reveal_file = reveal_file,
+          reveal_force_cwd = true,
+        })
+      end,
+      desc = "Reveal current file in Neo-tree",
+    },
+  },
+
   config = function(_, opts)
     require("neo-tree").setup(opts)
     vim.api.nvim_set_hl(0, "NeoTreeRootName", { bold = true, italic = false })
