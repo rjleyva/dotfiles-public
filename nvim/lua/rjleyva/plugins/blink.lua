e90a08bd71352ac36330db5143919e74ea1d88cb
RJ <rleyva@tutanota.com>
2025-06-12 17:51:16 -0400

    feat(blink): simplify config using static opts and add lazydev integration

--- nvim/lua/rjleyva/plugins/blink.lua
+++ nvim/lua/rjleyva/plugins/blink.lua
@@ -9,29 +9,35 @@ return {
       version = vim.g.lazyvim_blink_main and "*" or "1.*",
     },
   },
-  opts = function()
-    return {
-      keymap = { preset = "default" },
-      appearance = { nerd_font_variant = "mono" },
-      signature = { enabled = true },
-      fuzzy = { implementation = "prefer_rust_with_warning" },
-      completion = {
-        ghost_text = { enabled = true },
-        trigger = { show_on_keyword = true },
-        menu = { draw = { treesitter = { "lsp" } } },
-      },
-      cmdline = {
-        keymap = { preset = "inherit" },
-        completion = {
-          menu = { auto_show = false },
-          ghost_text = { enabled = false },
+  opts = {
+    sources = {
+      default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
         },
       },
-    }
-  end,
+    },
+    keymap = { preset = "default" },
+    appearance = { nerd_font_variant = "mono" },
+    signature = { enabled = true },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+    completion = {
+      ghost_text = { enabled = true },
+      trigger = { show_on_keyword = true },
+      menu = { draw = { treesitter = { "lsp" } } },
+    },
+    cmdline = {
+      keymap = { preset = "inherit" },
+      completion = {
+        menu = { auto_show = false },
+        ghost_text = { enabled = false },
+      },
+    },
+  },
   config = function(_, opts)
-    vim.schedule(function()
-      require("blink.cmp").setup(opts)
-    end)
+    require("blink.cmp").setup(opts)
   end,
 }
