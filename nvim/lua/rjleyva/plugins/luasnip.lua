62f56763a385f9605e456062516b2b083734c378
RJ <rleyva@tutanota.com>
2025-06-12 06:38:20 -0400

    refactor(luasnip): restructure plugin config to load VSCode snippets

--- nvim/lua/rjleyva/plugins/luasnip.lua
+++ nvim/lua/rjleyva/plugins/luasnip.lua
@@ -2,17 +2,21 @@ return {
   "L3MON4D3/LuaSnip",
   event = "InsertEnter",
   build = "make install_jsregexp",
+  dependencies = {
+    {
+      "rafamadriz/friendly-snippets",
+      lazy = true,
+    },
+  },
   config = function()
     local luasnip = require("luasnip")
+
     luasnip.config.set_config({
       history = true,
       updateevents = "TextChanged,TextChangedI",
     })
 
-    require("luasnip.loaders.from_vscode").lazy_load({
-      paths = { "./snippets" },
-    })
-
     require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } }) -- your own
   end,
 }
