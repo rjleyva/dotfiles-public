89a8815c9ab465156b6cd10761fa50dfdb8a87b2
RJ <rleyva@tutanota.com>
2025-06-12 18:24:16 -0400

    chore(neo-tree): reorder fields to align with lazy.nvim conventions

--- nvim/lua/rjleyva/plugins/neo-tree.lua
+++ nvim/lua/rjleyva/plugins/neo-tree.lua
@@ -16,6 +16,7 @@ return {
       lazy = true,
     },
   },
+
   opts = {
     sources = { "filesystem", "buffers", "git_status" },
     open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
