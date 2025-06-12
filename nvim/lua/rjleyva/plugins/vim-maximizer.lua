21c965819ff351d949f7cbedd959bbc6da0f9eb0
RJ <rleyva@tutanota.com>
2025-06-12 19:11:20 -0400

    chore(vim-maximizer): reorder plugin fields for clarity

--- nvim/lua/rjleyva/plugins/vim-maximizer.lua
+++ nvim/lua/rjleyva/plugins/vim-maximizer.lua
@@ -1,9 +1,9 @@
 return {
   "szw/vim-maximizer",
-  keys = {
-    { "<leader>ts", "<cmd>MaximizerToggle<CR>", desc = "Toggle Split Maximization" },
-  },
   init = function()
     vim.g.maximizer_set_default_mapping = 0
   end,
+  keys = {
+    { "<leader>ts", "<cmd>MaximizerToggle<CR>", desc = "Toggle Split Maximization" },
+  },
 }
