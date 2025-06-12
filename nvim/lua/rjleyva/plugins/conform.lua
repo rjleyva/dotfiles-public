9d9c1092d9e42befe1ad25b143a5fb456aa5f253
RJ <rleyva@tutanota.com>
2025-06-12 18:00:51 -0400

    refactor(conform): remove redundant pcall in config setup

--- nvim/lua/rjleyva/plugins/conform.lua
+++ nvim/lua/rjleyva/plugins/conform.lua
@@ -1,7 +1,6 @@
 return {
   "stevearc/conform.nvim",
   version = "v9.0.0",
-  enabled = true,
   event = { "BufReadPre", "BufNewFile" },
   opts = {
     formatters_by_ft = {
@@ -22,15 +21,10 @@ return {
       timeout_ms = 500,
     },
   },
+
   config = function(_, opts)
-    local ok, conform = pcall(require, "conform")
-    if not ok then
-      vim.notify("conform.nvim failed to load!", vim.log.levels.ERROR)
-      return
-    end
-
+    local conform = require("conform")
     conform.setup(opts)
-
     vim.keymap.set({ "n", "v" }, "<leader>cf", function()
       conform.format({ async = false, lsp_fallback = true })
     end, { desc = "Manually format file or range" })
