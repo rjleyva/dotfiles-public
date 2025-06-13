78fc635d1107787facbb0184f5704b4f1e055dcd
RJ <rleyva@tutanota.com>
2025-06-13 05:28:48 -0400

    refactor(alpha): comment out dashboard plugin for minimal setup

--- nvim/lua/rjleyva/plugins/alpha.lua
+++ nvim/lua/rjleyva/plugins/alpha.lua
@@ -1,41 +1,41 @@
 return {
-  "rjleyva/alpha-nvim",
-  event = "VimEnter",
-  enabled = (vim.fn.argc(-1) == 0),
-  opts = function()
-    local dashboard = require("alpha.themes.dashboard")
-
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-    }
-
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
-      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>FzfLua files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>FzfLua live_grep<CR>"),
-      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
-    }
-
-    return dashboard.config
-  end,
-  config = function(_, opts)
-    require("alpha").setup(opts)
-
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "alpha",
-      callback = function()
-        vim.opt_local.foldenable = false
-      end,
-    })
-  end,
+  -- "rjleyva/alpha-nvim",
+  -- event = "VimEnter",
+  -- enabled = (vim.fn.argc(-1) == 0),
+  -- opts = function()
+  --   local dashboard = require("alpha.themes.dashboard")
+  --
+  --   dashboard.section.header.val = {
+  --     [[                                                                       ]],
+  --     [[                                                                     ]],
+  --     [[       ████ ██████           █████      ██                     ]],
+  --     [[      ███████████             █████                             ]],
+  --     [[      █████████ ███████████████████ ███   ███████████   ]],
+  --     [[     █████████  ███    █████████████ █████ ██████████████   ]],
+  --     [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+  --     [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+  --     [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+  --     [[                                                                       ]],
+  --   }
+  --
+  --   dashboard.section.buttons.val = {
+  --     dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+  --     dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
+  --     dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>FzfLua files<CR>"),
+  --     dashboard.button("SPC fs", "  > Find Word", "<cmd>FzfLua live_grep<CR>"),
+  --     dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+  --   }
+  --
+  --   return dashboard.config
+  -- end,
+  -- config = function(_, opts)
+  --   require("alpha").setup(opts)
+  --
+  --   vim.api.nvim_create_autocmd("FileType", {
+  --     pattern = "alpha",
+  --     callback = function()
+  --       vim.opt_local.foldenable = false
+  --     end,
+  --   })
+  -- end,
 }
