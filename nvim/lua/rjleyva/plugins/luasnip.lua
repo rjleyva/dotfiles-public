8b9fbca0b0d120bb7cc2772def3e6b3825928670
RJ <rleyva@tutanota.com>
2025-06-15 16:44:12 -0400

    feat(luasnip): enable autosnippets

--- nvim/lua/rjleyva/plugins/luasnip.lua
+++ nvim/lua/rjleyva/plugins/luasnip.lua
@@ -14,6 +14,7 @@ return {
     luasnip.config.set_config {
       history = true,
       updateevents = 'TextChanged,TextChangedI',
+      enable_autosnippets = true,
     }
 
     require('luasnip.loaders.from_vscode').lazy_load()
