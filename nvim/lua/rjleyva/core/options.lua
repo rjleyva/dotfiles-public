315e80efc9dd43c944a953eef26f229ea3b5a0a9
RJ <rleyva@tutanota.com>
2025-06-15 03:41:10 -0400

    feat(options): set list mode, spelllang, timeoutlen, and wildmode for better editing experience

--- nvim/lua/rjleyva/core/options.lua
+++ nvim/lua/rjleyva/core/options.lua
@@ -1,64 +1,76 @@
+-- Leader key
 vim.g.mapleader = " "
 
--- UI settings
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.numberwidth = 3
+-- General UI
+vim.opt.background = "dark"
+vim.opt.breakindent = true
+vim.opt.cmdheight = 0
 vim.opt.cursorline = false
+vim.opt.laststatus = 0
+vim.opt.number = true
+vim.opt.numberwidth = 3
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
+vim.opt.showcmd = false
+vim.opt.showmode = false
 vim.opt.signcolumn = "yes:1"
 vim.opt.statuscolumn = "%l%s"
-vim.opt.title = true
 vim.opt.termguicolors = true
-vim.opt.background = "dark"
+vim.opt.title = true
 vim.opt.wrap = false
-vim.opt.breakindent = true
-vim.opt.showcmd = false
-vim.opt.cmdheight = 0
-vim.opt.laststatus = 0
-vim.opt.scrolloff = 10
 
--- Mouse and split behavior
+-- Mouse and splits
 vim.opt.mouse = ""
+vim.opt.splitbelow = true
 vim.opt.splitkeep = "cursor"
 vim.opt.splitright = true
-vim.opt.splitbelow = true
 
 -- Tabs and indentation
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
 vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
+vim.opt.tabstop = 2
 
 -- Search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
 vim.opt.hlsearch = true
+vim.opt.ignorecase = true
 vim.opt.inccommand = "split"
+vim.opt.smartcase = true
 
--- File handling
-vim.opt.backup = false
-vim.opt.swapfile = false
-vim.opt.shell = "zsh"
-vim.opt.backspace = { "start", "eol", "indent" }
+-- Spell, timeout, and list chars
+vim.opt.list = true
+vim.opt.spelllang = { "en" }
+vim.opt.timeoutlen = vim.g.vscode and 1000 or 300
 
 -- Command and path behavior
-vim.opt.shortmess:append("I")
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.formatoptions:append({ "r" })
 vim.opt.path:append({ "**" })
+vim.opt.shortmess:append("I")
 vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.wildmode = "longest:full,full"
+
+-- File handling
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.backup = false
+vim.opt.shell = "zsh"
+vim.opt.swapfile = false
+
+-- Global variables
+vim.g.deprecation_warnings = true
 
 -- Disable unused built-in plugins
-vim.g.loaded_gzip = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zipPlugin = 1
 vim.g.loaded_2html_plugin = 1
-vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_gzip = 1
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_zipPlugin = 1
 
--- Terminal styling (italic + underline tweaks)
+-- Terminal styling tweaks (italics, underlinea)
+-- I got this from craftzdog
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
 vim.cmd("set t_ZH=[3m")
