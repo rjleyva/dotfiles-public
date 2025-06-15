b3fca1f42cdede6cdbf88e86475f7d585b8affbd
RJ <rleyva@tutanota.com>
2025-06-15 04:51:51 -0400

    feat(keys): add <leader>N to open a new empty buffer

--- nvim/lua/rjleyva/core/keys.lua
+++ nvim/lua/rjleyva/core/keys.lua
@@ -3,8 +3,8 @@ local keys = vim.keymap
 -- Exit Insert Mode
 keys.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
--- Basic Navigation & Editing (inspired by Josean Martinez's Neovim config)
--- including my structure
+-- (inspired by Josean Martinez's Neovim config) including Neovim structure
+-- Navigation & Editing
 keys.set("n", "x", '"_x', { desc = "Delete char without copying to register" })
 keys.set("n", "<leader>+", "<C-a>", { desc = "Increment number under cursor" })
 keys.set("n", "<leader>-", "<C-x>", { desc = "Decrement number under cursor" })
@@ -28,13 +28,21 @@ keys.set("n", "<leader>ch", "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR
   desc = "Clear highlights, update diff, and redraw screen",
 })
 
+-- Launch lazy.nvim
 keys.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Open Lazy.nvim plugin manager" })
+
+-- Exit
 keys.set("n", "<leader>X", "<cmd>qa<cr>", { desc = "Quit all windows and exit" })
 
+-- New file
+-- NOTE: This opens a new unnamed buffer.
+--       to save it, run `:w filename.ext` (e.g., :w notes.txt).
+keys.set("n", "<leader>N", "<cmd>enew<cr>", { desc = "New File" })
+
+-- Commenting
 keys.set("n", "<leader>co", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Insert line below with comment" })
 keys.set("n", "<leader>cO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Insert line above with comment" })
 
--- Commenting
 keys.set("n", "<leader>cc", function()
   local commentstring = vim.bo.commentstring:match("^(.*)%%s")
   local line = vim.api.nvim_get_current_line()
